a5206a39a64de732848526faec60a2c2a5d3bfcd
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-21 11:58:43 -0600

    feat: validation for create project endpoint

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,8 +1,25 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
+router.post('/',
+
+    body('projectName')
+        .notEmpty().withMessage('Project Name is required'),
+
+    body('clientName')
+        .notEmpty().withMessage('Client Name is required'),
+
+    body('description')
+        .notEmpty().withMessage('Description is required'),
+
+    handleInputErrors,
+
+    ProjectController.createProject
+);
 router.get('/', ProjectController.getAllProject);
 
 export default router;
