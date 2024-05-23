2232fdcc19b87f04bcba93e0f25e475131204258
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-23 11:06:59 -0600

    feat: endpoint to update the status

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -93,4 +93,13 @@ router.delete('/:projectId/tasks/:taskId',
     TaskController.deleteTask
 );
 
+router.post('/:projectId/tasks/:taskId/status',
+
+    param('taskId').isMongoId().withMessage('Invalid ID'),
+    body('status')
+        .notEmpty().withMessage('Status is required'),
+    handleInputErrors,
+    TaskController.updateStatus
+);
+
 export default router;
