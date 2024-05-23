cec66e027c6990a2c62ee5c38c6f9dab126b4331
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-23 10:37:18 -0600

    feat: method that update a task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -65,4 +65,31 @@ export class TaskController {
             });
         }
     };
+
+    static updateTask = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params;
+            const task = await Task.findByIdAndUpdate(taskId, req.body);
+
+            if (!task) {
+                const error = new Error('Task not found');
+                return res.status(404).json({
+                    error: error.message
+                });
+            }
+            if (task.project.toString() !== req.project.id) {
+                const error = new Error('Invalid action');
+                return res.status(400).json({
+                    error: error.message
+                });
+            }
+
+            res.send('Tasks updated successfully');
+
+        } catch (error) {
+            res.status(500).json({
+                error: 'An error occurred'
+            });
+        }
+    };
 }
