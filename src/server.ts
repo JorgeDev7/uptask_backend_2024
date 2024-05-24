ae867fb68254dd8cbd28d6316e764175403a5d40
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-24 10:40:13 -0600

    feat: add cors and config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { json } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
 
@@ -7,6 +9,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// CORS
+app.use(cors(corsConfig));
 
 // Enabling JSON
 app.use(json());
