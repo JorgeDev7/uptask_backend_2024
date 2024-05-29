2f1621b91f009ef72d453cb41b77322ef07e9e02
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-29 11:07:58 -0600

    feat: adding new route for authRoutes

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 connectDB();
@@ -21,6 +22,7 @@ app.use(morgan('dev'));
 app.use(json());
 
 // Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 
 export default app;
