f93d55fda91e2c9bafc0e61b34c5a18b739f3445
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-31 11:50:21 -0600

    feat: email method to send a new email to reset password

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -24,4 +24,22 @@ export class AuthEmail {
 
         console.log('Mensaje Enviado', info.messageId);
     };
+
+    static sendPasswordResetToken = async ({ email, name, token }: IEmail) => {
+        const info = await transporter.sendMail({
+            from: 'Uptask <admin@uptask.com>',
+            to: email,
+            subject: 'UpTask - Reestablece tu password',
+            text: 'UpTask - Reestablece tu password',
+            html: `<p>Hola ${name}, has solicitado reestablecer tu password.</p>
+
+                <p>Visita el siguiente enlace:</p>
+                <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
+                <p>E ingresa el código: <b>${token}</b></p>
+                <p>Este token expira en 10 minutos</p>
+            `
+        });
+
+        console.log('Mensaje Enviado', info.messageId);
+    };
 }
