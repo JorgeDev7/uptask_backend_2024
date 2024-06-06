3d3982993b15646b5d5ba1643a29cd1537bf50be
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-06 12:10:01 -0600

    feat: method to add a member to a specific project

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -5,15 +5,47 @@ export class TeamMemberController {
     static findMemberByEmail = async (req: Request, res: Response) => {
         const { email } = req.body;
 
-        // Find User
-        const user = await User.findOne({ email }).select('id email name');
-        if (!user) {
-            const error = new Error('Usuario no encontrado');
-            return res.status(404).json({
-                error: error.message
-            });
-        }
+        try {
+            // Find User
+            const user = await User.findOne({ email }).select('id email name');
+            if (!user) {
+                const error = new Error('Usuario no encontrado');
+                return res.status(404).json({
+                    error: error.message
+                });
+            }
 
-        res.json(user);
+            res.json(user);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    static addMemberById = async (req: Request, res: Response) => {
+        const { id } = req.body;
+        try {
+
+            const user = await User.findById(id).select('id');
+            if (!user) {
+                const error = new Error('Usuario no encontrado');
+                return res.status(404).json({
+                    error: error.message
+                });
+            }
+
+            if (req.project.team.some(team => team.toString() === user.id.toString())) {
+                const error = new Error('El usuario ya ha sido añadido al Proyecto');
+                return res.status(409).json({
+                    error: error.message
+                });
+            }
+
+            req.project.team.push(user.id);
+            await req.project.save();
+
+            res.send('Usuario agregado correctamente');
+        } catch (error) {
+            console.log(error);
+        }
     };
 }
