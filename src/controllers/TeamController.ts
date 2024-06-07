0991fd818180a16a98ec72fe0ea0bfe6c732fd8a
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-07 10:04:45 -0600

    refactor: Project model imported

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import Project from "../models/Project";
 
 export class TeamMemberController {
     static findMemberByEmail = async (req: Request, res: Response) => {
