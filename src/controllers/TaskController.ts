6b8e68297ac5b77fcb0a5115d4476378139098fc
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-11 10:50:15 -0600

    feat: update and get who made a changed on a task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -88,6 +88,11 @@ export class TaskController {
 
             const { status } = req.body;
             req.task.status = status;
+            if (status === 'pending') {
+                req.task.completedBy = null;
+            } else {
+                req.task.completedBy = req.user.id;
+            }
             await req.task.save();
 
             res.send('Estado actualizado');
