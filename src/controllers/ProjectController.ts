486abf8e4f6b48a6ec3882f855be4da6a9355175
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-11 09:42:14 -0600

    feat: displaying projects to the team

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -24,7 +24,8 @@ export class ProjectController {
         try {
             const projects = await Project.find({
                 $or: [
-                    { manager: { $in: req.user.id } } //condition to get only the projects of the current user
+                    { manager: { $in: req.user.id } }, //condition to get only the projects of the current user
+                    { team: { $in: req.user.id } } // Condition to show all projects to users who are part of the project team.
                 ]
             });
             res.json(projects);
@@ -47,7 +48,7 @@ export class ProjectController {
                 });
             }
 
-            if (project.manager.toString() !== req.user.id.toString()) {
+            if (project.manager.toString() !== req.user.id.toString() && !project.team.includes(req.user.id)) {
                 const error = new Error('Acción no válida');
                 return res.status(404).json({
                     error: error.message
