0943e6039ed5f122cf8e034f33d75bd9beaf59ec
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-12 11:13:10 -0600

    feat: endpoint to get all notes

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -145,4 +145,8 @@ router.post('/:projectId/tasks/:taskId/notes',
     NoteController.createNote
 );
 
+router.get('/:projectId/tasks/:taskId/notes',
+    NoteController.getTaskNotes
+);
+
 export default router;
