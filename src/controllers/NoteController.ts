0c7214fe362c58ad7148f3a44ea496fb75447d09
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-12 10:58:16 -0600

    feat: method to create a note

---
+++ src/controllers/NoteController.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from 'express';
+import Note, { INote } from '../models/Note';
+
+export class NoteController {
+    static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
+        const { content } = req.body;
+        const note = new Note();
+        note.content = content;
+        note.createdBy = req.user.id;
+        note.task = req.task.id;
+
+        req.task.notes.push(note.id);
+
+        try {
+            await Promise.allSettled([req.task.save(), note.save()]);
+            res.send('Nota Creada Correctamente');
+        } catch (error) {
+            res.status(500).json({
+                error: 'Hubo un error'
+            });
+        }
+    };
+}
