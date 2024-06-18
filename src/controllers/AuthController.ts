5ad031aea54e969a5a6fc05a6ed70629c212dd80
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-18 12:07:30 -0600

    feat: method to change password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -272,4 +272,28 @@ export class AuthController {
             });
         }
     };
+
+    static updateCurrentUserPassword = async (req: Request, res: Response) => {
+        const { current_password, password } = req.body;
+
+        const user = await User.findById(req.user.id);
+        const isPasswordCorrect = await checkPassword(current_password, user.password);
+
+        if (!isPasswordCorrect) {
+            const error = new Error('La contraseña actual es incorrecta');
+            return res.status(401).json({
+                error: error.message
+            });
+        }
+
+        try {
+            user.password = await hashPassword(password);
+            await user.save();
+            res.send('La contraseña se modificó correctamente');
+        } catch (error) {
+            res.status(500).json({
+                error: 'Hubo un error'
+            });
+        }
+    };
 }
